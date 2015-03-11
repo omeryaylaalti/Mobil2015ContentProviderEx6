05790a65ba700309d755992891a8a8ca7e54244c
Özlem YAYLAALTI <ozlemyaylaalti@Ozlems-MacBook-Pro.local>
2015-03-11 17:02:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/omeryaylaalti/mobil2015contentproviderex6/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.omeryaylaalti.mobil2015contentproviderex6;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView contactView = (TextView) findViewById(R.id.contactview);
+
+        Cursor cursor = getContacts();
+
+        while (cursor.moveToNext()) {
+
+            String displayName = cursor.getString(cursor
+                    .getColumnIndex(ContactsContract.Data.DISPLAY_NAME));
+            contactView.append("Name: ");
+            contactView.append(displayName);
+            contactView.append("\n");
+        }
+
+    }
+    private Cursor getContacts() {
+        // Run query
+        Uri uri = ContactsContract.Contacts.CONTENT_URI;
+        String[] projection = new String[] { ContactsContract.Contacts._ID,
+                ContactsContract.Contacts.DISPLAY_NAME };
+        String selection = ContactsContract.Contacts.IN_VISIBLE_GROUP + " = '"
+                + ("1") + "'";
+        String[] selectionArgs = null;
+        String sortOrder = ContactsContract.Contacts.DISPLAY_NAME
+                + " COLLATE LOCALIZED ASC";
+
+
+        return managedQuery(uri, projection, selection, selectionArgs,
+                sortOrder);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
